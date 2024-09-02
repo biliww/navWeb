5b201e5e1c2689b75cb567074d0cfe389c42c226
wangpl <843145936@qq.com>
2024-09-02 14:43:02 +0800

    feat: 添加safari限制

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -46,17 +46,17 @@ export default {
   },
 }
 
-// if (typeof window !== 'undefined') {
-//   // detect browser, add to class for conditional styling
-//   const browser = navigator.userAgent.toLowerCase()
-//   if (browser.includes('chrome')) {
-//     document.documentElement.classList.add('browser-chrome')
-//   } else if (browser.includes('firefox')) {
-//     document.documentElement.classList.add('browser-firefox')
-//   } else if (browser.includes('safari')) {
-//     document.documentElement.classList.add('browser-safari')
-//   }
-// }
+if (typeof window !== 'undefined') {
+  // detect browser, add to class for conditional styling
+  const browser = navigator.userAgent.toLowerCase()
+  if (browser.includes('chrome')) {
+    document.documentElement.classList.add('browser-chrome')
+  } else if (browser.includes('firefox')) {
+    document.documentElement.classList.add('browser-firefox')
+  } else if (browser.includes('safari')) {
+    document.documentElement.classList.add('browser-safari')
+  }
+}
 
 // Speed up the rainbow animation on home page
 function updateHomePageStyle(value: boolean) {
